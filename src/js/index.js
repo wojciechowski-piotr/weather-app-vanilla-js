8aeb303ec8af00a534d47f0f7825f6ac3677bec4
wojciechowski-piotr <pedro9493@gmail.com>
2020-04-01 20:59:49 +0200

    connection to api, fetched some sample data

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,27 @@
-console.log("Hello stranger!");
+console.log("Weather App");
+
+let cityName;
+cityName = 'Legnica';
+const apiKey = '774384fa58572680f7bc37ab01913d7d';
+const apiCall = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+
+
+console.log(apiCall);
+
+fetch(apiCall)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then((data) => {
+        console.log(`City: ${data.name} (${data.sys.country})`);
+        console.log(`Temp: ${Math.round(data.main.temp, 0)} C`);
+        data.weather.forEach(el => {
+            console.log(`Weather: ${el.main}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });    
