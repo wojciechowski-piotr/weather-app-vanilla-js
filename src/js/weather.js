662b7a1669d97d99e8ded334db35264499f24322
wojciechowski-piotr <pedro9493@gmail.com>
2020-04-11 18:27:29 +0200

    moved app logic to function in weather.js file, added geolocation feature

--- src/js/weather.js
+++ src/js/weather.js
@@ -1,24 +1,28 @@
-import {
-    format
-} from 'date-fns';
+import { format } from 'date-fns';
 
-class Weather {
-    constructor() {
-        this.params = (new URL(document.location)).searchParams;
-        this.city = this.params.get('city');
-        this.cityName = null;
-        this.apiKey = '774384fa58572680f7bc37ab01913d7d';
-        this.apiQuery = `q=${this.cityName}`;
-        this.apiCall = `https://api.openweathermap.org/data/2.5/weather?${this.apiQuery}&units=metric&appid=${this.apiKey}`;
-    }
-    findLocation() {
-        function success() {
+function weatherApp() {
+
+    let params = (new URL(document.location)).searchParams;
+    let city = params.get('city');
+    let cityName = city;
+
+
+    const apiKey = '774384fa58572680f7bc37ab01913d7d';
+    let apiQuery = `q=${cityName}`;
+    let apiCall = `https://api.openweathermap.org/data/2.5/weather?${apiQuery}&units=metric&appid=${apiKey}`;
+
+    if (city === null) {
+
+        function success(position) {
             const lat = position.coords.latitude;
             const lon = position.coords.longitude;
 
             apiQuery = `lat=${lat}&lon=${lon}`;
+            apiCall = `https://api.openweathermap.org/data/2.5/weather?${apiQuery}&units=metric&appid=${apiKey}`;
 
-            fetch(this.apiCall)
+            console.log("success -> apiQuery", apiQuery)
+
+            fetch(apiCall)
                 .then((response) => {
                     if (!response.ok) {
                         throw new Error('Network response was not ok');
@@ -26,7 +30,24 @@ class Weather {
                     return response.json();
                 })
                 .then((data) => {
-                    this.displayData();
+                    const cityCnt = document.querySelector('.weather__city');
+                    const iconCnt = document.querySelector('.weather__icon');
+                    const icon = new Image();
+                    const tempCnt = document.querySelector('.weather__temp');
+                    const descCnt = document.querySelector('.weather__desc');
+
+                    cityCnt.innerHTML = `<h3>${data.name}, ${data.sys.country}</h3>`;
+                    tempCnt.innerHTML = `<p>${Math.round(data.main.temp, 0)}&#176;C</p>`;
+                    data.weather.forEach(el => {
+                        icon.src = `https://openweathermap.org/img/wn/${el.icon}@2x.png`;
+                        iconCnt.appendChild(icon);
+                        descCnt.innerHTML = `<p>${el.description}</p>`;
+                    });
+
+                    const currentDate = format(new Date(), 'dd-MMM-yyyy, HH:mm');
+                    const timeCnt = document.querySelector('.weather__time');
+
+                    timeCnt.innerHTML = `${currentDate}`;
                 })
                 .catch((error) => {
                     console.error('Error:', error);
@@ -35,6 +56,7 @@ class Weather {
 
         function error() {
             console.log('Unable to retrieve your location');
+            window.alert('Unable to retrieve your location');
         }
 
         if (!navigator.geolocation) {
@@ -42,57 +64,39 @@ class Weather {
         } else {
             navigator.geolocation.getCurrentPosition(success, error);
         }
+
     }
 
-    byCityName() {
+    fetch(apiCall)
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => {
+            const cityCnt = document.querySelector('.weather__city');
+            const iconCnt = document.querySelector('.weather__icon');
+            const icon = new Image();
+            const tempCnt = document.querySelector('.weather__temp');
+            const descCnt = document.querySelector('.weather__desc');
 
-        this.city === null ? this.cityName = 'london' : this.cityName = city;
-
-        fetch(this.apiCall)
-            .then((response) => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then((data) => {
-                this.displayData();
-            })
-            .catch((error) => {
-                console.error('Error:', error);
+            cityCnt.innerHTML = `<h3>${data.name}, ${data.sys.country}</h3>`;
+            tempCnt.innerHTML = `<p>${Math.round(data.main.temp, 0)}&#176;C</p>`;
+            data.weather.forEach(el => {
+                icon.src = `https://openweathermap.org/img/wn/${el.icon}@2x.png`;
+                iconCnt.appendChild(icon);
+                descCnt.innerHTML = `<p>${el.description}</p>`;
             });
-    }
 
-    displayData() {
-        const cityCnt = document.querySelector('.weather__city');
-        const iconCnt = document.querySelector('.weather__icon');
-        const icon = new Image();
-        const tempCnt = document.querySelector('.weather__temp');
-        const descCnt = document.querySelector('.weather__desc');
+            const currentDate = format(new Date(), 'dd-MMM-yyyy, HH:mm');
+            const timeCnt = document.querySelector('.weather__time');
 
-        cityCnt.innerHTML = `<h3>${data.name}, ${data.sys.country}</h3>`;
-        tempCnt.innerHTML = `<p>${Math.round(data.main.temp, 0)}&#176;C</p>`;
-        data.weather.forEach(el => {
-            icon.src = `https://openweathermap.org/img/wn/${el.icon}@2x.png`;
-            iconCnt.appendChild(icon);
-            descCnt.innerHTML = `<p>${el.description}</p>`;
+            timeCnt.innerHTML = `${currentDate}`;
+        })
+        .catch((error) => {
+            console.error('Error:', error);
         });
-
-        const currentDate = format(new Date(), 'dd-MMM-yyyy, HH:mm');
-        const timeCnt = document.querySelector('.weather__time');
-
-        timeCnt.innerHTML = `${currentDate}`;
-    }
-
-    startCall() {
-        /* if (this.cityName === null) {
-            this.findLocation();
-        } else {
-            this.byCityName();
-        } */
-
-        this.byCityName();
-    }
 }
 
-export default Weather;
+export default weatherApp;
